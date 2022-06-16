6d6d6aa848be378256d190fa49aed2c7fd3b09e9
Lyn <lynfinman@gmail.com>
2022-06-16 10:42:55 -0400

    put 4 sushi on belt

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,18 +1,27 @@
 import React, { useState, useEffect } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
 function SushiContainer(props) {
   const [sushis, setSushis] = useState([]);
+  const [sushiStart, setSushiStart] = useState(0);
+  const [beltSushi, setBeltSushi] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:3001/sushis")
       .then((r) => r.json())
       .then((sushis) => setSushis(sushis));
   }, []);
-  console.log("Loaded sushi - ", sushis.length);
+
+  useEffect(() => {
+    setBeltSushi(sushis.slice(sushiStart, sushiStart + 4));
+  }, [sushis, sushiStart]);
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
+      {beltSushi.map((sushi) => (
+        <Sushi sushi={sushi} key={sushi.id} />
+      ))}
       <MoreButton />
     </div>
   );
